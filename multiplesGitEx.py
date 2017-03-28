bec047a2688931434d574fc276e9bbb9915b80e7
Noopur Bhatt <nbhatt2020@pingry.org>
2017-03-28 13:50:13 -0400

    Wrote code for basic doubling.

---
+++ multiplesGitEx.py
@@ -0,0 +1,9 @@
+
+# Ask for user input.
+userNum = input("Tell me a number. ")
+
+# Convert to float.
+userNum = float(userNum)
+
+# Do the doubling.
+print("Double that number is {}.".format(userNum * 2))
